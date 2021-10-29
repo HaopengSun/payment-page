7fc01ec1d9208254cc77b3858a52ba94b1469151
serendipity-harpoon <sunhaopeng2016@gmail.com>
2021-10-28 20:01:43 -0400

    payment finished and redirect to success page

--- charge.php
+++ charge.php
@@ -7,23 +7,25 @@ $dotenv->safeLoad();
 
 $STRIPE_SECRET_KEY = $_ENV['STRIPE_SECRET_KEY'];
 
-$stripe = new \Stripe\StripeClient($STRIPE_SECRET_KEY);
+\Stripe\Stripe::setApiKey($STRIPE_SECRET_KEY);
 
 $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
 
 $first_name = $POST['first_name'];
 $last_name = $POST['last_name'];
 $email = $POST['email'];
-$token = $POST['stripeToke'];
+$token = $POST['stripeToken'];
 
-$customer = $stripe->customers->create([
-    'email' => $email,
-    'source' => $token,
-]);
+$customer = \Stripe\Customer::create(array(
+    "email" => $email,
+    "source" => $token
+));
 
 $charge = \Stripe\Charge::create(array(
     "amount" => 5000,
     "currency" => "usd",
-    "description" => "intro yo react course",
+    "description" => "Intro To React Course",
     "customer" => $customer->id
 ));
+
+header('Location: success.php?tid='.$charge->id.'&product='.$charge->description);
